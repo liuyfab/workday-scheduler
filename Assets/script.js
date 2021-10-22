cef58714743e98af25765ec1f981cddc06e840d7
liuyfab <liuyongfeng83@gmail.com>
2021-10-21 22:11:51 -0500

    1st version js code

--- Assets/script.js
+++ Assets/script.js
@@ -1,30 +1,33 @@
 
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
-var currentHour = moment().format('h:mm'); 
+// Moment Current time 
+var currentHour = moment();
+
 //on click of save button store values in Local Storage using LocalStorage.setItem()
 $(".saveBtn").on('click', function(){
-    //1. grab hour 
-    var hour = $(this).siblings(".hour").text(); 
-    //console.log(hour); 
+    //1. grab hour
+    var hour = $(this).siblings(".hour").text();
+    console.log(hour);
     //2. grab the information from textarea
-    var description = $(this).siblings(".description").val();
-    //console.log(description);
-    //3. Store it LS in key,value format 
-    localStorage.setItem(hour, description);
+    var event = $(this).siblings(".event-text").val();
+    console.log(event);
+    //3. Store it LS in key,value format
+    localStorage.setItem(hour, event);
 });
 
-//start loop 
-$('.row').each(function(description){  
-//Page Loads u want to grab the stored value from LS and show ininside the textarea
-    var text = localStorage.getItem(description.val);
-    //console.log("Grab value from LS", text); 
-
-    //Background color changed based on the current time whether its is past, prest or future. 
-    if(currentHour > hour){ 
-        $(".row").addClass('.present')
-    }
-    else if(currentHour<hour){
-        $(".row").addClass('.past')
-    }
-})
-//end loop 
+$('.row').each(function(event){  
+    //grab the stored value from LS and show ininside the textarea
+        var text = localStorage.getItem(event);
+        //console.log("Grab value from LS", text);
+      //Background color changed based on the current time whether its is past, prest or future.
+      var hour = $(this).siblings(".hour").text();  
+      if(currentHour.isAfter(hour)){
+          $(".row.event").addClass('.future');
+      }
+      else if(currentHour.isBefore(hour)){
+          $(".row.event").addClass('.past');  
+      }
+      else{
+          $(".row.event").addClass('.present');
+        }
+    })
